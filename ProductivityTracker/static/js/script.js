87e1d3d8c5f0873d787710cf7cee8be318170021
milinddinesh <milinddinesh@gmail.com>
2024-06-02 13:17:55 +0530

    add functionality for quotes

--- ProductivityTracker/static/js/script.js
+++ ProductivityTracker/static/js/script.js
@@ -1,36 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     fetch('/task/completion_percentage')
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log('API Response:', data); // Log the response to debug
-//             const progressValue = parseFloat(data.completion_percentage).toFixed(2); // Ensure the value is a float and limit to 2 decimal places
-//             updateProgressBar(progressValue);
-//         })
-//         .catch(error => console.error('Error fetching progress:', error));
-// });
-
-// function updateProgressBar(percentage) {
-//     if (typeof percentage === 'undefined' || isNaN(percentage)) {
-//         console.error('Percentage is undefined or not a number');
-//         return;
-//     }
-
-//     const progressValueElement = document.querySelector('.progress-value');
-//     const progressLeftBar = document.querySelector('.progress-left .progress-bar');
-//     const progressRightBar = document.querySelector('.progress-right .progress-bar');
-
-//     const rotationValue = (percentage / 100) * 360;
-
-//     if (percentage <= 50) {
-//         progressRightBar.style.transform = `rotate(${rotationValue}deg)`;
-//         progressLeftBar.style.transform = 'rotate(0deg)';
-//     } else {
-//         progressRightBar.style.transform = 'rotate(180deg)';
-//         progressLeftBar.style.transform = `rotate(${rotationValue - 180}deg)`;
-//     }
-
-//     progressValueElement.textContent = `${percentage}%`;
-// }
 document.addEventListener("DOMContentLoaded", function() {
     fetch('/task/completion_percentage')
         .then(response => response.json())
@@ -41,6 +8,24 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error('Error fetching progress:', error));
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Generate a random integer between 1 and 22
+    const randomInteger = Math.floor(Math.random() * 22) + 1;
+
+    // Fetch the quote using the random integer
+    fetch(`/quote/${randomInteger}/`)
+        .then(response => response.json())
+        .then(data => {
+            console.log('API Response:', data); // Log the response to debug
+
+            // Update the quote text and author
+            document.querySelector('.quote-text').textContent = data.quote;
+            document.querySelector('.blog-post-bottom').textContent = data.author;
+        })
+        .catch(error => console.error('Error fetching quote:', error));
+});
+
 
 function updateProgressBar(percentage) {
     if (typeof percentage === 'undefined' || isNaN(percentage)) {
