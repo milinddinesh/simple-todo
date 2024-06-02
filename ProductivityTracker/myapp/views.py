42e1146682ca522c683fa11c6fec1c87c7142f76
milinddinesh <milinddinesh@gmail.com>
2024-06-02 13:42:09 +0530

    add context for in-progress

--- ProductivityTracker/myapp/views.py
+++ ProductivityTracker/myapp/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from Tasks.models import Task
 
 # Create your views here.
 
@@ -15,4 +16,9 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        in_progress_tasks = Task.objects.filter(owner=self.request.user, progress_status=Task.IN_PROGRESS)
+        context['in_progress_tasks'] = in_progress_tasks
+        return context
 
